6289ad6825a85399c4b16e0f6db6545c0394da3d
Dipesh Shivrame <dipeshshivrame@gmail.com>
2024-01-11 11:57:42 +0530

    added lock on variable to avoid race conditions

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync"
 )
 
+var servers = []string{
+	"http://localhost:3000",
+	"http://localhost:3001",
+	"http://localhost:3002",
+	"http://localhost:3003",
+}
+
+var nextServer = 0
+var mutex sync.Mutex
+
 func handler(rw http.ResponseWriter, req *http.Request) {
-	rpUrl, _ := url.Parse("http://localhost:3000")
+	mutex.Lock()
+	rpUrl, _ := url.Parse(servers[nextServer%len(servers)])
+	fmt.Println(nextServer)
+	nextServer = (nextServer + 1) % len(servers)
+	mutex.Unlock()
 	proxy := httputil.NewSingleHostReverseProxy(rpUrl)
 	proxy.ServeHTTP(rw, req)
 }
